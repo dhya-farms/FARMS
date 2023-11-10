099661232ee679378bdf6cc6a13506cdbe95317d
dhya-farms <sandbox@dhya.in>
2023-11-10 14:41:10 +0530

    testing 08

--- permission.sh
+++ permission.sh
@@ -1,27 +1,19 @@
-#!/bin/bash
-
-# Set the base directory for the project
-BASE_DIR="/var/lib/jenkins/workspace/FARMS"
-
-# Create the 'nginx' group if it doesn't exist and add users to it
+# Create a new group called 'nginx'
 sudo groupadd nginx
+
+# Add 'www-data', 'root', and 'jenkins' to the 'nginx' group
 sudo usermod -a -G nginx root
 sudo usermod -a -G nginx www-data
 sudo usermod -a -G nginx jenkins
 
 # Change the group ownership of the project directory and its contents to 'nginx'
-sudo chown -R root:nginx /var/lib/jenkins
 
-# Set the appropriate permissions
-sudo chmod 755 /var/lib/jenkins
-sudo chmod 755 /var/lib/jenkins/workspace
-sudo chmod 770 -R "$BASE_DIR"
+sudo chown root:nginx /var/lib/jenkins/workspace/FARMS
+sudo chown -R root:nginx /var/lib/jenkins/workspace/FARMS
 
-# If there are specific subdirectories within the project that need different permissions, set them here
-sudo chmod 770 -R "$BASE_DIR/staticfiles"
-sudo chmod 770 -R "$BASE_DIR/app"
-sudo chmod 770 -R "$BASE_DIR/FARMS"
+sudo chown -R root:nginx /var/lib/jenkins/workspace/FARMS/staticfiles
+sudo chown -R root:nginx /var/lib/jenkins/workspace/FARMS/app
+sudo chown -R root:nginx /var/lib/jenkins/workspace/FARMS/FARMS
 
-
-sudo ufw allow 80
-sudo ufw allow 443
+sudo chmod 770 /var/lib/jenkins/workspace/FARMS
+sudo chmod 770 -R /var/lib/jenkins/workspace/FARMS/staticfiles
