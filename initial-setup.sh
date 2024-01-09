dbbc734536f129ecfcf34f83f2e696a5530154b9
dhya-farms <sandbox@dhya.in>
2024-01-09 14:36:37 +0530

    Revert "redeploying to new instance"
    This reverts commit 33d505a310712686c6842037814523e685f8035e.

--- initial-setup.sh
+++ initial-setup.sh
@@ -15,7 +15,6 @@
 #sudo systemctl restart jenkins
 #sudo systemctl enable jenkins
 #sudo systemctl status jenkins
-#sudo apt-get install nginx supervisor
 
 # OS dependencies
 sudo apt-get install software-properties-common
